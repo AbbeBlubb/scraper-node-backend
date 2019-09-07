02ac2ace7af1d22e1441af48849727cf0c8745a7
Alberto Chávez Pérez <andalusier@pm.me>
2019-09-08 01:29:37 +0200

    Refactoring

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-const request = require('request-promise');
-const cheerio = require('cheerio');
-const fs = require('fs');
 const express = require('express');
-const mongoose = require('mongoose');
-const Header = mongoose.model('Header');
-const helpers = require('./helpers');
 const cors = require('cors');
 const routes = require('./routes/index');
 
@@ -19,53 +13,5 @@ app.set('json spaces', 2);
 
 // Use rotes from separate file
 app.use('/', routes);
-
-
-
-
-
-// Initialize the model
-const headerModel = new Header({title: 'Lördag', url: 'Pinn'});
-headerModel.save()
-
-// Vars with dates
-const today = helpers.getDate();
-const latestDate = fs.readFileSync('./data/latest-date.txt', 'utf-8')
-
-// Vars for scraping
-const urlAftonbladet = 'https://www.aftonbladet.se';
-const scrapeResults = []
-const anchorWithClassAndAttribute = "a._3xv_Q[data-test-id='teaser']";
-const classForTitle = '._1Qq8L';
-
-
-async function scrapeDomTree(urlToScrape) {
-  
-  if (today !== latestDate) {
-    try {
-      const htmlResponse = await request.get(urlToScrape);
-      const $ =  cheerio.load(htmlResponse);
-
-      $(anchorWithClassAndAttribute).each((index, element) => {
-        const title = $(element).find(classForTitle).text();
-        const link = urlAftonbladet + $(element).attr('href');
-        const scrapeResult = { title, link };
-        scrapeResults.push(scrapeResult);
-      })
-
-      // Save the data
-      // fs.writeFileSync(file, data[, options])
-      // JSON.stringify(value[, replacer[, space]])
-      fs.writeFileSync('./data/latest-scrape.js', JSON.stringify(scrapeResults, null, 2), 'utf-8');
-      fs.writeFileSync('./data/latest-date.js', );
-    } 
-    
-    catch (err) {
-      console.error(err);
-    }
-  }
-}
-
-scrapeDomTree(urlAftonbladet);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,66 @@
 const express = require('express');
 const router = express.Router();
+const Header = mongoose.model('Header');
+const helpers = require('./helpers');
+const request = require('request-promise');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const mongoose = require('mongoose');
+
+
+// Initialize the model
+const headerModel = new Header({title: 'Lördag', url: 'Pinn'});
+headerModel.save()
+
+// Vars with dates
+const today = helpers.getDate();
+const latestDate = fs.readFileSync('./data/latest-date.txt', 'utf-8')
+
+// Vars for scraping
+const urlAftonbladet = 'https://www.aftonbladet.se';
+const scrapeResults = []
+const anchorWithClassAndAttribute = "a._3xv_Q[data-test-id='teaser']";
+const classForTitle = '._1Qq8L';
+
+
+async function scrapeDomTree(urlToScrape) {
+  
+  if (today !== latestDate) {
+    try {
+      const htmlResponse = await request.get(urlToScrape);
+      const $ =  cheerio.load(htmlResponse);
+
+      $(anchorWithClassAndAttribute).each((index, element) => {
+        const title = $(element).find(classForTitle).text();
+        const link = urlAftonbladet + $(element).attr('href');
+        const scrapeResult = { title, link };
+        scrapeResults.push(scrapeResult);
+      })
+
+      // Save the data
+      // fs.writeFileSync(file, data[, options])
+      // JSON.stringify(value[, replacer[, space]])
+      fs.writeFileSync('./data/latest-scrape.js', JSON.stringify(scrapeResults, null, 2), 'utf-8');
+      fs.writeFileSync('./data/latest-date.js', );
+
+      // Returnera data
+    } 
+    
+    catch (err) {
+      console.error(err);
+    }
+  } else {
+      // Returnera data
+  }
+}
+
+
+
 
 // Routes. Exported at the end of the file
 router.get('/test', function(req, res){
+
+    //data to user = kör scrapern och få tillbaka svar. scrapeDomTree(urlAftonbladet);
     res.json('hej')
 });
 
