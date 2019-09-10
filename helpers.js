ae141a3c528747b3eedd0b81f0fb7478aca3df27
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-09-10 21:07:40 +0200

    Securing app if the DB returns an empty array

--- helpers.js
+++ helpers.js
@@ -15,8 +15,9 @@ exports.getTodaysDate = function() {
 exports.getLatestDBdate = async function() {
   // .find returns an empty array if no matches are found. .findOne returns null if no matches are found. Here all documents are needed.
   const latestDBDateAsObject = await Scrape.find({}, {date: 1}).sort({date: -1}).limit(1);
-  // If latestDBDateASObject === [], then latestDBDateAsString will be null
-  const latestDBDateAsString =  latestDBDateAsObject === !null ? (latestDBDateAsObject[0].date).toISOString().substring(2, 10) : null;
+  // If latestDBDateASObject === [], then latestDBDateAsString will be null. Else, get the date
+  const latestDBDateAsString = (latestDBDateAsObject.length == 0) ? null : (latestDBDateAsObject[0].date).toISOString().substring(2, 10);
+  console.log(latestDBDateAsString)
   return latestDBDateAsString;
 }
 
