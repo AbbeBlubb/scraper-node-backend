98d49fbe2fa7919f0982c6ddad007008a62518ac
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-09-10 20:02:01 +0200

    Comparing dates, refactoring

--- helpers.js
+++ helpers.js
@@ -1,14 +1,24 @@
-const moment = require('moment');
-const mongoose = require('mongoose');
-//const Scrape = mongoose.model('Scrape');
+const Scrape = require('./models/Scrape.js');
 
+// Time for the console, to see when changes have been done
 exports.getTime = function() {
   const currentdate = new Date();
   return (currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds());
 };
 
-exports.getDate = function() {
-  return moment().format('YY-MM-DD');
+// Today's date to compare with the latest DB date
+exports.getTodaysDate = function() {
+  return (new Date()).toISOString().substring(2, 10);
 }
 
+// Latest date for write to DB
+exports.getLatestDBdate = async function() {
+  const latestDBDateAsObject = await Scrape.find({}, {date: 1}).sort({date: -1}).limit(1);
+  const latestDBDateAsString = await (latestDBDateAsObject[0].date).toISOString().substring(2, 10);
+  return latestDBDateAsString;
+}
 
+// URL:s for scraping
+exports.getUrlAftonbladet = function() {
+   return 'https://www.aftonbladet.se'
+}
--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,19 @@
-//const fs = require('fs');
 const request = require('request-promise');
 const cheerio = require('cheerio');
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const Scrape = mongoose.model('Scrape');
+const Scrape = require('../models/Scrape.js');
 const helpers = require('../helpers');
 
 
- async function getLatestDBdate () {
-  const latestDBdateObject = await Scrape.find({}, {date: 1}).sort({date: -1}).limit(1);
-  const latestDBdate = latestDBdateObject[0].date
-  //const shortDate = latestDBdate.split('T')[0].substring(2)
-  console.log('test', latestDBdate);
-  return latestDBdate
-}
-getLatestDBdate()
-
-// Initialize the model
-//const scrapeModel = new Scrape()
-
-// Vars with dates
-const today = helpers.getDate();
-//const latestDate = fs.readFileSync('./data/latest-date.txt', 'utf-8')
-const latestDate = '19-09-09'
-console.log(today, latestDate)
-
-// URL:s for scraping
-const urlAftonbladet = 'https://www.aftonbladet.se';
-
-
 async function scrapeDomTree(urlToScrape) {
-  
-  if (today !== latestDate) {
+
+// Vars with dates. They are in the async function because of a needed await
+const todaysDate = helpers.getTodaysDate();
+const latestDate = await helpers.getLatestDBdate()
+console.log(todaysDate, latestDate)
+
+  if (todaysDate !== latestDate) {
     try {
 
       // Vars for scraping Aftonbladet 
@@ -54,12 +35,6 @@ async function scrapeDomTree(urlToScrape) {
         scrapeResults.push(scrapeResult);
       })
 
-      // Save the data + scraping date, in case you want to experiment and avoid repeated scrapes
-      // fs.writeFileSync(file, data[, options])
-      // JSON.stringify(value[, replacer[, space]])
-      //fs.writeFileSync('./data/latest-scrape.json', JSON.stringify(scrapeResults), 'utf-8');
-      //fs.writeFileSync('./data/latest-date.txt', today, 'utf-8' );
-
       // Construct the document
       const scrapeModel = new Scrape({
         date: Date(),
@@ -79,10 +54,6 @@ async function scrapeDomTree(urlToScrape) {
     }
 
   } else {
-      // Returnera senaste sparade data. Parsa till JS iom att response sker i json för att matcha scraperns return
-      //console.log('!! Loaded local file');
-      //return JSON.parse(fs.readFileSync('./data/latest-scrape.json', 'utf-8'));
-
       // Return the latest written document from the DB
       return await Scrape.find({}).sort({date: -1}).limit(1);
     }
@@ -93,7 +64,7 @@ async function scrapeDomTree(urlToScrape) {
 router.get('/aftonbladet', async function(req, res){
 
   // Run the scraper and load the data
-  const data = await scrapeDomTree(urlAftonbladet);
+  const data = await scrapeDomTree(helpers.getUrlAftonbladet);
   
   // Response to JSON
   res.json(data)
